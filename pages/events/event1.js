8d3591ee25696ad4948240053b23952359dfffd3
robdcampbell <rdeancampbell@gmail.com>
2023-01-23 22:13:49 -0500

    Routing with next.js

---
+++ pages/events/event1.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Event 1</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Events (home)</h1>
+    </div>
+  );
+};
+
+export default Page;
