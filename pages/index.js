689dab48bb54aebb64ad75cfeb82053ecb62cd84
robdcampbell <rdeancampbell@gmail.com>
2023-01-23 22:16:40 -0500

    Adding Navigation with next.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>test</h1>
+        <header>
+          <nav>
+            <img src="" alt="" />
+            <a href="/">Events</a>
+            <a href="/events">Home</a>
+            <a href="/about">About Us</a>
+          </nav>
+        </header>
       </main>
     </>
   );
