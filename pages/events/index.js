4e9507e55dd5b0ba6bb69888a3ac64c13dc32e59
robdcampbell <rdeancampbell@gmail.com>
2023-01-25 23:04:28 -0500

    Contiuing Nav

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,14 @@
 const Page = () => {
   return (
     <div>
+      <header>
+        <nav>
+          <img src="" alt="" />
+          <a href="/">Events</a>
+          <a href="/events">Home</a>
+          <a href="/about">About Us</a>
+        </nav>
+      </header>
       <h1>Events (home)</h1>
     </div>
   );
